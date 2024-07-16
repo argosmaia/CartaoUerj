b4b5be285f634e3ce9c74413df36a9cec61b79de
Argos Maia <argosmaia@pop-os.localdomain>
2024-07-16 12:03:59 -0300

    First attempt

---
+++ src/main/java/com/cartaouerj/cartaouerj/CartaouerjApplication.java
@@ -0,0 +1,13 @@
+package com.cartaouerj.cartaouerj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaouerjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaouerjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartaouerj/cartaouerj/CartaouerjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartaouerj.cartaouerj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartaouerjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
